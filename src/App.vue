9804142c47772ff267f48f484065de113e1f015a
Алим Зулфугаров <kragol.zulfugarov@yandex.ru>
2023-03-02 00:26:50 +0300

    сosmetic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    Working
-  </div>
+    <div class="all">
+        <h1>Навигация по БГТУ</h1>
+        <MyForm
+            @ok="init"
+        />
+        <MyMap
+
+        />
+    </div>
 </template>
 
 <script>
+import MyForm from '@/components/MyForm.vue'
+import MyMap from '@/components/MyMap.vue'
 
+export default {
+    components: { MyForm, MyMap },
+    data() {
+        return {
+            from: '',
+            to: ''
+        }
+    },
+    methods: {
+        init(from, to) {
+            this.from = from,
+            this.to = to,
+            console.log(from, to)
+        }
+    }
+}
 </script>
 
-<style>
-
+<style scoped>
+    .all {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,72 @@
+<template>
+    <form @submit.prevent>
+        <div>
+            <input type="text" placeholder="Откуда"
+                v-model="from">
+             → 
+            <input type="text" placeholder="Куда"
+                v-model="to">
+        </div>
+        <span v-if="exception">Заполните все поля!</span>
+        <button @click="buildRoute">
+            Проложить маршрут
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            from: '',
+            to: '',
+            exception: false
+        }
+    },
+    methods: {
+        buildRoute() {
+            if (this.from.trim() && this.to.trim()) {
+                this.$emit('ok', this.from, this.to)
+                this.from = '',
+                this.to = '',
+                this.exception = false;
+            }
+            else
+                this.exception = true;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    input {
+        padding: 10px 15px;
+        border: 1px gray solid;
+        border-radius: 10px;
+    }
+    span {
+        color: red;
+        font-weight: bold;
+        font-size: 20px;
+        margin-top: 20px;
+    }
+    button {
+        width: max-content;
+        margin: 20px 0;
+        padding: 10px 20px;
+        background: none;
+        color: teal;
+        border: 2px gray solid;
+        border-radius: 10px;
+    }
+    button:hover {
+        background-color: gainsboro;
+        color: black;
+        border: 2px black solid;
+    }
+</style>
